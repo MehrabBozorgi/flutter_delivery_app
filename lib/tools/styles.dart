63f32c11fda6f7ff52957e16c6cb130a129bbdfe
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-11-11 18:28:09 +0330

    delivery_app

---
+++ lib/tools/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const textStyle1=TextStyle(color: darkColor,fontSize:24,fontFamily: 'bold' );
+const textStyle2=TextStyle(color: darkColor,fontSize:20,fontFamily: 'bold' );
+const textStyle3=TextStyle(color: darkColor,fontSize:18,fontFamily: 'bold' );
+const textStyle4=TextStyle(color: darkColor,fontSize:16,fontFamily: 'bold' );
+const textStyle5=TextStyle(color: darkColor,fontSize:18,fontFamily: 'normal' );
+const textStyle6=TextStyle(color: darkColor,fontSize:16,fontFamily: 'normal' );
+//
+final textStyle7=TextStyle(color: Colors.grey.shade700,fontSize:16,fontFamily: 'bold' );
+const textStyle8=TextStyle(color: Colors.grey,fontSize:16,fontFamily: 'normal' );
+//
+const textStyle9=TextStyle(color: Colors.white,fontSize:16,fontFamily: 'normal',wordSpacing: 1 );
+const textStyle10=TextStyle(color: Colors.white,fontSize:20,fontFamily: 'bold' );
+//
+const textStyle11=TextStyle(color: primaryColor,fontSize:18,fontFamily: 'bold' );
+final textStyle12=TextStyle(color: Colors.yellow.shade700,fontSize:14,fontFamily: 'bold' );
