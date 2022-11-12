0903f215ed3064ca329d28b2320823879e827bdb
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-11-12 10:59:18 +0330

    delivery_app

---
+++ lib/tools/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffbd0202);
+const backgroundColor = Color(0xffebebf3);
+
+const darkColor = Color(0xFF212121);
+const whiteColor = Color(0xfffaf9f9);
+final greyColor = Colors.grey.shade300;
